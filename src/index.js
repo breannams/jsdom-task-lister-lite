5c464cf55c688ce66e6ca3fca2158ecba6fb3b0f
breannams <breannamarie_s@hotmail.com>
2021-06-28 16:56:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form  =  document.getElementById("create-task-form")
+let list = document.getElementById("tasks")
+
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+let newTaskDesc = document.getElementById("new-task-description").value
+let li = document.createElement("li")
+
+li.innerText = newTaskDesc
+
+list.appendChild(li)
+})
 });
